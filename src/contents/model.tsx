5ebfecd681805f708cd38235a23a85b3cdaa9b0e
Jarvis <mengda636431@outlook.com>
2024-09-20 01:10:30 +0800

    [fea]修改了model和software的样式

--- src/contents/model.tsx
+++ src/contents/model.tsx
@@ -1,12 +1,75 @@
 export function Model() {
-  return (
-    <>
-        <h2>Overview</h2>
-        <h2>Part1 Simulation of biofilm growth in gut</h2>
-        <h2>Part2 Multivalent adhesion model</h2>
-        <h2>Part3 Metabolic flow prediction based on FBA</h2>
-        <h2>Part4 CondonF44</h2>
-        <h2>Reference</h2>
-    </>
-  );
+    return (
+        <main style={{backgroundColor: '#FFF8F0', minHeight: '100vh'}}>
+            <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '20px'}}>
+                <img
+                    src="https://static.igem.wiki/teams/5376/viruse1.png"
+                    alt="Left Image"
+                    style={{maxWidth: '100px', height: 'auto'}}
+                />
+
+                <img
+                    src="https://static.igem.wiki/teams/5376/model.png"
+                    alt="TITLE"
+                    style={{width: '250px', height: '110px', objectFit: 'cover'}}
+                />
+
+
+                <img
+                    src="https://static.igem.wiki/teams/5376/viruse2.png"
+                    alt="Right Image"
+                    style={{maxWidth: '100px', height: 'auto'}}
+                />
+            </div>
+
+
+            <div style={{display: 'flex', justifyContent: 'space-between', padding: '20px', height: "600px"}}>
+
+                <aside
+                    style={{
+                        width: '30%',
+                        backgroundColor: '#FFF4E6',
+                        padding: '20px',
+                        height: '100%',
+                        justifyContent: 'space-between',
+                        borderRadius: '10px'
+                }}>
+                    <h3 style={{fontSize: '28px', color: '#c67e38', fontWeight: 'bold'}}>Overview</h3>
+                    <ul style={{listStyleType: 'none', padding: '0'}}>
+                        <li style={{fontSize: '22px', color: '#c67e38', fontWeight: 'bold', marginBottom: '10px'}}>
+                            Part1 Simulation of biofilm growth in gut
+                        </li>
+                        <li style={{fontSize: '22px', color: '#c67e38', fontWeight: 'bold', marginBottom: '10px'}}>
+                            Part2 Multivalent adhesion model
+                        </li>
+                        <li style={{fontSize: '22px', color: '#c67e38', fontWeight: 'bold', marginBottom: '10px'}}>
+                            Part3 Metabolic flow prediction based on FBA
+                        </li>
+                        <li style={{fontSize: '22px', color: '#c67e38', fontWeight: 'bold', marginBottom: '10px'}}>
+                            Part4 CondonF44
+                        </li>
+                        <li style={{fontSize: '22px', color: '#c67e38', fontWeight: 'bold', marginBottom: '10px'}}>
+                            Reference
+                        </li>
+                    </ul>
+                </aside>
+
+                <div
+                    style={{
+                        width: '65%',
+                        padding: '20px',
+                        backgroundImage: 'url(https://static.igem.wiki/teams/5376/textbox-background.png)',
+                        backgroundSize: 'cover'
+                    }}>
+                    <img
+                        src="https://static.igem.wiki/teams/5376/overview.png"
+                        alt="Overview"
+                        style={{width: '50%', height: 'auto'}}
+                    />
+                </div>
+            </div>
+        </main>
+    );
 }
+
+
--- src/contents/software.tsx
+++ src/contents/software.tsx
@@ -1,11 +1,66 @@
 export function Software() {
+    return (
+        <main style={{backgroundColor: '#FFF8F0', minHeight: '100vh'}}>
+            <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '20px'}}>
+                <img
+                    src="https://static.igem.wiki/teams/5376/viruse1.png"
+                    alt="Left Image"
+                    style={{maxWidth: '100px', height: 'auto'}}
+                />
 
-  return (
-    <>
-      <h2>Overview</h2>
-      <h2>Part1 Metabolite database of probiotics—MDOP</h2>
-      <h2>Part2 Software package—MDOP Tool</h2>
-      <h2>Reference</h2>
-    </>
-  );
+                <img
+                    src="https://static.igem.wiki/teams/5376/software.png"
+                    alt="TITLE"
+                    style={{width: '400px', height: '110px', objectFit: 'cover'}}
+                />
+
+
+                <img
+                    src="https://static.igem.wiki/teams/5376/viruse2.png"
+                    alt="Right Image"
+                    style={{maxWidth: '100px', height: 'auto'}}
+                />
+            </div>
+
+
+            <div style={{display: 'flex', justifyContent: 'space-between', padding: '20px', height: "600px"}}>
+
+                <aside style={{
+                    width: '30%',
+                    backgroundColor: '#FFF4E6',
+                    padding: '20px',
+                    height: '100%',
+                    justifyContent: 'space-between',
+                    borderRadius: '10px'
+                }}>
+                    <h3 style={{fontSize: '28px', color: '#c67e38', fontWeight: 'bold'}}>Overview</h3>
+                    <ul style={{listStyleType: 'none', padding: '0'}}>
+                        <li style={{fontSize: '22px', color: '#c67e38', fontWeight: 'bold', marginBottom: '10px'}}>
+                            Part1. Metabolite database of probiotics—MDOP
+                        </li>
+                        <li style={{fontSize: '22px', color: '#c67e38', fontWeight: 'bold', marginBottom: '10px'}}>
+                            Part2. Software package—MDOP Tool
+                        </li>
+                        <li style={{fontSize: '22px', color: '#c67e38', fontWeight: 'bold', marginBottom: '10px'}}>
+                            Reference
+                        </li>
+                    </ul>
+                </aside>
+
+                <div
+                    style={{
+                        width: '65%',
+                        padding: '20px',
+                        backgroundImage: 'url(https://static.igem.wiki/teams/5376/textbox-background.png)',
+                        backgroundSize: 'cover'
+                    }}>
+                    <img
+                        src="https://static.igem.wiki/teams/5376/overview.png"
+                        alt="Overview"
+                        style={{width: '50%', height: 'auto'}}
+                    />
+                </div>
+            </div>
+        </main>
+    );
 }
