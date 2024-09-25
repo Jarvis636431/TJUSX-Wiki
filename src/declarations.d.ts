da3fba8b52e404db5d481e488863e5fca9deae59
Jarvis <mengda636431@outlook.com>
2024-09-25 23:06:01 +0800

    [fea]修复了bug，实现了边框的内外圆角，点击跳转有待优化，图片还在上传中

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -7,20 +7,24 @@ declare module '*.module.css' {
 // declarations.d.ts
 declare module 'react-scroll' {
     // 声明Link组件的props类型
+    import React from "react";
+
     export interface LinkProps {
-        activeClass?: string;       // 当元素在屏幕上时的类名
-        to: string;                 // 滚动到的元素名称
-        spy?: boolean;              // 是否监视元素并激活类名
-        smooth?: boolean;           // 是否平滑滚动
-        offset?: number;            // 滚动的偏移量
-        duration?: number;          // 滚动持续的时间
-        delay?: number;             // 滚动的延迟时间
-        isDynamic?: boolean;        // 动态容器大小是否影响滚动
-        onSetActive?: () => void;   // 当元素激活时的回调
-        onSetInactive?: () => void; // 当元素不激活时的回调
-        ignoreCancelEvents?: boolean; // 是否忽略滚动取消事件
-        containerId?: string;       // 容器的ID
-        children?: React.ReactNode; // 子元素
+        activeClass?: string,
+        to: string,
+        spy?: boolean,
+        smooth?: boolean,
+        offset?: number,
+        duration?: number,
+        delay?: number,
+        isDynamic?: boolean,
+        onSetActive?: () => void,
+        onSetInactive?: () => void,
+        ignoreCancelEvents?: boolean,
+        containerId?: string,
+        children?: React.ReactNode,
+        onMouseOver?: (e) => string,
+        onMouseOut?: (e) => string
     }
 
     // 声明Element组件的props类型
