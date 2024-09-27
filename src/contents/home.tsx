d1babc1fd3c157072412e7ac28dc8e0b058e3e81
git <chiyongjie@tju.edu.cn>
2024-09-28 02:21:10 +0800

    添加了home页面的图层效果

--- src/contents/home.tsx
+++ src/contents/home.tsx
@@ -1,19 +1,25 @@
+import React, { useState, useEffect } from 'react';
 export function Home() {
+    const [scrollY, setScrollY] = useState(0);
+    useEffect(() => {
+        const handleScroll = () => {
+          setScrollY(window.scrollY);
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
     return (
         <>
-            <main style={{
-                backgroundImage: 'url(https://static.igem.wiki/teams/5376/wiki-background.png)',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                minHeight: '100vh',
-                position: 'relative', // 设置父容器为 relative，方便定位子元素
-            }}>
+            <main style={{ backgroundColor: '#fff8f0', minHeight: '100vh' }}>
                 {/* 叠加的第一张图片 */}
                 <img
                     src="https://static.igem.wiki/teams/5376/title.png"
                     alt="Overlay Title"
                     style={{
-                        position: 'absolute',
+                        position: 'fixed',
                         top: '50%',  // 根据需要调整图片的位置
                         left: '50%',
                         transform: 'translate(-50%, -50%)',  // 让图片居中
@@ -27,11 +33,77 @@ export function Home() {
                     src="https://static.igem.wiki/teams/5376/subtitle.png"
                     alt="Overlay Subtitle"
                     style={{
-                        position: 'absolute',
+                        position: 'fixed',
                         top: '55%',  // 位置调整为靠近第一张图片的下方
                         left: '50%',
                         transform: 'translate(-50%, -50%)',  // 让图片居中
                         maxWidth: '100%', // 确保图片在较小屏幕上不会超出宽度
+                        height: 'auto',
+                    }}
+                />
+                <img
+                    src="https://static.igem.wiki/teams/5376/home/m1left.png"
+                    alt="Overlay Subtitle"
+                    style={{
+                        position: 'absolute',
+                        top: '52%',  // 位置调整为靠近第一张图片的下方
+                        left: `${-scrollY * 0.3}px`,
+                        maxWidth: '100%', // 确保图片在较小屏幕上不会超出宽度
+                        height: 'auto',
+                    }}
+                />
+                <img
+                    src="https://static.igem.wiki/teams/5376/home/m1right.png"
+                    alt="Overlay Subtitle"
+                    style={{
+                        position: 'absolute',
+                        top: '25%',  // 位置调整为靠近第一张图片的下方
+                        left: `${scrollY * 0.3+70}px`,
+                        maxWidth: '100%', // 确保图片在较小屏幕上不会超出宽度
+                        height: 'auto',
+                    }}
+                />
+                <img
+                    src="https://static.igem.wiki/teams/5376/home/m2l.png"
+                    alt="Overlay Subtitle"
+                    style={{
+                        position: 'absolute',
+                        top: '52%',  // 位置调整为靠近第一张图片的下方
+                        left: `${-scrollY * 0.5}px`,
+                        maxWidth: '100%', // 确保图片在较小屏幕上不会超出宽度
+                        height: 'auto',
+                    }}
+                />
+                <img
+                    src="https://static.igem.wiki/teams/5376/home/m2r.png"
+                    alt="Overlay Subtitle"
+                    style={{
+                        position: 'absolute',
+                        top: '55%',  // 位置调整为靠近第一张图片的下方
+                        left: `${scrollY * 0.5+10}px`,
+                        maxWidth: '100%', // 确保图片在较小屏幕上不会超出宽度
+                        height: 'auto',
+                    }}
+                />
+                <img
+                    src="https://static.igem.wiki/teams/5376/home/m3r.png"
+                    alt="Overlay Subtitle"
+                    style={{
+                        position: 'absolute',
+                        top: '55%',  // 位置调整为靠近第一张图片的下方
+                        left: `${scrollY * 0.8+520}px`,
+                        maxWidth: '70%', // 确保图片在较小屏幕上不会超出宽度
+                        height: 'auto',
+                    }}
+                />
+                <img
+                    src="https://static.igem.wiki/teams/5376/home/m3l.png"
+                    alt="Overlay Subtitle"
+                    style={{
+                        position: 'absolute',
+                        top: '58%',  // 位置调整为靠近第一张图片的下方
+                        left: `${-scrollY * 0.8}px`,
+                        maxWidth: '70%', // 确保图片在较小屏幕上不会超出宽度
                         height: 'auto',
                     }}
                 />
